2804101ee8a0a9a4bb28ecb844be0b57e62520f2
Nathan Sankary <nesankar@mtu.edu>
2022-06-05 10:39:52 -0700

    add density print out

--- scripts/exp_1.py
+++ scripts/exp_1.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
-from tqdm import tqdm
-import concurrent.futures  # TODO: processing is slow, do it in parallel
+# import concurrent.futures  # TODO: processing is slow, do it in parallel
 
 """Simple script to explore loading an image and some analyses"""
 
@@ -13,7 +12,7 @@ from src import basic_utils, image_io
 
 
 if __name__ == "__main__":
-    img_path = root / "resources" / "misc_data" / "second_reference"
+    img_path = root / "resources" / "misc_data" / "first_reference"
     img_type = "jpeg"
     images = image_io.load_dir_images(img_path, img_type)
 
@@ -26,8 +25,9 @@ if __name__ == "__main__":
         # do some simple tooling
         image.plot_analyzed_plant()
         print("\n")
-        print(f"The total area of the {image.name} plant is {round(image.plant_area_in2, 2)}.")
+        print(f"The total area of the {image.name} plant is {round(image.plant_area_in2, 2)} inches^2.")
         print(f"The {image.name} plant is {round(image.plant_length_in, 2)} inches by {round(image.plant_width_in, 2)} inches @ max.")
+        print(f"The {image.name} plant density {round(image.plant_area_in2 / (image.plant_width_in * image.plant_length_in) * 100, 2)}%.")
         print("\n")
         image.get_plant_color_spectrum(plot=True)
 
